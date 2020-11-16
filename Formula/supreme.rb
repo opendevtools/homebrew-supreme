8d828719cc6b757fb6a4444900b12921c2a161d6
Rickard Natt och Dag <nattochdag@hey.com>
2020-11-16 11:44:49 +0100

    feat(release): v1.3.3

--- Formula/supreme.rb
+++ Formula/supreme.rb
@@ -1,9 +1,9 @@
 class Supreme < Formula
-  version '1.3.2'
+  version '1.3.3'
   desc "A CLI tool that helps you get up an running fast"
   homepage "https://github.com/opendevtools/supreme-rs"
   url "https://github.com/opendevtools/supreme-rs/releases/download/#{version}/supreme-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "dd7f395bdeb80c2e5180d25aed2bbf3aac55290136e12dd47bac7c6f29c19c78"
+  sha256 "02fee5400e7504cb62b02610bae0ac70ca12a003e4f4d7c88cf9e93397e6c928"
 
   def install
     bin.install "supreme"

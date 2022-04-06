465c9320679431eab9b42818f809e260f5f788aa
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-04-06 10:17:42 +0200

    fix: add version
    New Homebrew versions were published when they shouldn't have been

--- Formula/supreme.rb
+++ Formula/supreme.rb
@@ -1,5 +1,5 @@
 class Supreme < Formula
-  version ""
+  version "1.26.0"
   desc "A CLI tool that helps you get up an running fast"
   homepage "https://github.com/opendevtools/supreme-rs"
   url "https://github.com/opendevtools/supreme-rs/releases/download/#{version}/supreme-#{version}-x86_64-apple-darwin.tar.gz"
